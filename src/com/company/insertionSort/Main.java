395069810f8b75bc49c4f4d5e75d5672697f9204
Aske21 <veledarevica@yahoo.com>
2021-03-30 20:59:30 +0200

    added different main classes

--- src/com/company/insertionSort/Main.java
+++ src/com/company/insertionSort/Main.java
@@ -1,11 +1,8 @@
-package com.company;
+package com.company.insertionSort;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
-
 
     }
 }
--- src/com/company/shellSort/Main.java
+++ src/com/company/shellSort/Main.java
@@ -1,11 +1,7 @@
-package com.company;
+package com.company.shellSort;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
-
-
 
     }
 }
