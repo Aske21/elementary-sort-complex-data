555c1102098400d74becdfb66a36cacea56a083f
Aske21 <veledarevica@yahoo.com>
2021-03-30 20:50:17 +0200

    added insertionSort class

--- src/com/company/insertionSort/insertionSort.java
+++ src/com/company/insertionSort/insertionSort.java
@@ -1,4 +1,16 @@
 package com.company.insertionSort;
 
-public class insertionSort {
+public class insertionSort extends AbstractSort {
+
+    public static void sort(int[] elements) {
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (less(elements[j], elements[j - 1])){
+                    swap(elements, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
